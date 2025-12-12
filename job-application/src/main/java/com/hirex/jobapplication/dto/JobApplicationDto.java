0c9cd06a44e0b1144ba5cb18d0a1f6844cbaae59
johnscottini <jonathanoliveirascottini8@gmail.com>
2025-12-11 22:44:38 -0300

    [FEAT]: Ajustando dtos do job application

--- job-application/src/main/java/com/hirex/jobapplication/dto/JobApplicationDto.java
+++ job-application/src/main/java/com/hirex/jobapplication/dto/JobApplicationDto.java
@@ -1,7 +1,18 @@
 package com.hirex.jobapplication.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.hirex.jobapplication.domain.enums.ApplicationStatus;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class JobApplicationDto {
+
+    private String id;
+    private Long candidateId;
+    private Long vacancyId;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime appliedDate;
+    private ApplicationStatus status;
 }
--- job-application/src/main/java/com/hirex/jobapplication/dto/JobApplicationResumoDto.java
+++ job-application/src/main/java/com/hirex/jobapplication/dto/JobApplicationResumoDto.java
@@ -1,7 +1,18 @@
 package com.hirex.jobapplication.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.hirex.jobapplication.domain.enums.ApplicationStatus;
 import lombok.Data;
+
+import java.time.LocalDateTime;
 
 @Data
 public class JobApplicationResumoDto {
+
+    private String id;
+    private Long candidateId;
+    private Long vacancyId;
+    private ApplicationStatus status;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy HH:mm:ss")
+    private LocalDateTime appliedDate;
 }
